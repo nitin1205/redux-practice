3b2ee00fd6a39d2fff996073bf5359c235edc202
Nitin Singh <vortechcorp@gmail.com>
2023-01-26 23:24:39 +0530

    created songList and songDetail component and also setup redux store and created songReducer and selectedsong reducer and combined componets to appropriate reducer and actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import SongList from './components/SongList';
+import SongDetail from './components/SongDetail';
+import './App.css';
 
 function App() {
-  
 
-  return (
-    <div>app</div>
-      )
-}
+return (
+  <div className='ui container grid'>
+    <div className="ui row">
+      <div className="column eight wide">
+        <SongList />
+      </div>
+      <div className="column eight wide">
+        <SongDetail />
+      </div>
+    </div>
+  </div>
+  )
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './App'
+import reducers from './reducers';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStore(reducers)}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
